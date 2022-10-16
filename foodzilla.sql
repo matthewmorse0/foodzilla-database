59671bb18a189147f6bf169e367fb7f675226f4d
matthewmorse0 <66086952+matthewmorse0@users.noreply.github.com>
2022-10-16 14:45:56 -0500

    Implemented wait times and idea for seating arrangments

--- foodzilla.sql
+++ foodzilla.sql
@@ -11,28 +11,35 @@ CREATE TABLE RESTAURANT (
   rname        varchar(25) not null,
   rid    int not null,
   mgrid      char(9) not null,
+  waitTime      int not null,
+  menuLink char(50) not null,
+  -- diningTables is a string where each character is a digit from 0-9, 0 representing 10
+  -- Each digit represents a table and how many people can be seated there, iterating through rows top to bottom, left to right
+  diningTables char(20),
+  -- freeTables is a string of 0s (taken) and 1s (free) indicating whether the corresponding table in diningTables is taken or free
+  freeTables char(20),
   CONSTRAINT pk_Restaurant primary key (rid)
   -- CONSTRAINT uk_rname UNIQUE (rname) 
 );
 
-DROP TABLE IF EXISTS MENU;
-CREATE TABLE MENU (
-  menuid      char(4) not null,
-  rid      int not null,
-  items char(100),
-  CONSTRAINT pk_Menu primary key (menuid),
-  CONSTRAINT fk_Menu foreign key (rid) references RESTAURANT (rid)
-);
+-- DROP TABLE IF EXISTS MENU;
+-- CREATE TABLE MENU (
+--   menuid      char(4) not null,
+--   rid      int not null,
+--   items char(100),
+--   CONSTRAINT pk_Menu primary key (menuid),
+--   CONSTRAINT fk_Menu foreign key (rid) references RESTAURANT (rid)
+-- );
 
-INSERT INTO RESTAURANT VALUES ('Arby\'s', 1, '333445555');
-INSERT INTO RESTAURANT VALUES ('Chili\'s', 2, '987654321');
-INSERT INTO RESTAURANT VALUES ('Olive Garden', 3, '123456789');
-INSERT INTO RESTAURANT VALUES ('Wendy\'s', 4, '111111119');
+INSERT INTO RESTAURANT VALUES ('King Street Pub', 1, '333445555', 30, 'www.ksp.com', '4455228', '1100111');
+INSERT INTO RESTAURANT VALUES ('Chili\'s', 2, '987654321', 20, 'www.chilis.com', '82235', '10001');
+INSERT INTO RESTAURANT VALUES ('Olive Garden', 3, '123456789', 10, 'www.olives.com', '622554', '101011');
+INSERT INTO RESTAURANT VALUES ('Dion\'s', 4, '111111119', 10, 'www.dions.com', '8084442222', '1110010110');
 
-INSERT INTO MENU VALUES ('1111', 1, 'Beef, Sandwich, The Meats');
-INSERT INTO MENU VALUES ('2222', 2, 'Crispy Honey Chipotle Chicken Crispers, Quesadilla');
-INSERT INTO MENU VALUES ('3333', 3, 'Unlimited Soup/Salad/Breadsticks, Alfredo, Family');
-INSERT INTO MENU VALUES ('4444', 4, '4 for 4, Nuggs');
+-- INSERT INTO MENU VALUES ('1111', 1, 'Beef, Sandwich, The Meats');
+-- INSERT INTO MENU VALUES ('2222', 2, 'Crispy Honey Chipotle Chicken Crispers, Quesadilla');
+-- INSERT INTO MENU VALUES ('3333', 3, 'Unlimited Soup/Salad/Breadsticks, Alfredo, Family');
+-- INSERT INTO MENU VALUES ('4444', 4, '4 for 4, Nuggs');
 
 SELECT * FROM RESTAURANT;
-SELECT * FROM MENU;
+-- SELECT * FROM MENU;
