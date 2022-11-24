c43eb365794b2931c44dad471f66f193d7ee5eed
matthewmorse0 <66086952+matthewmorse0@users.noreply.github.com>
2022-11-24 13:43:18 -0700

    corrected seating queries

--- api.py
+++ api.py
@@ -13,49 +13,35 @@ def get_all_restaurants():
     restaurants = db.get_restaurants()
     return jsonify({'restaurants': restaurants})
 
-@app.route("/view_info", methods=['GET'])
-def restaurant_storage():
-    # rid = json.loads(request.get_json)
-    print('request=', request.data)
-    rid = request.get_json(force=True).get('rid')
-    info = db.get_info(rid)
-    return jsonify({'rest_info': info})
-
-# @app.route("/update_free", methods=['POST'])
-# def update_free_tables():
-#     rid = request.get_json(force=True).get('rid')
-#     freeTables = request.get_json(force=True).get('free')
-#     db.update_open_tables(rid, freeTables)
-#     return {'message': 'updated free tables'}
-
 @app.route("/update_layout", methods=['POST'])
 def update_seating_layout():
-    rid = request.get_json(force=True).get('rid')
-    seating = request.get_json(force=True).get('tables')
+    rid = request.args.get('rid')
+    seating = request.args.get('tables')
     freeTables = ''
     for i in seating:
         if i == '|':
             freeTables += '|'
+        elif i == '0':
+            freeTables += '0'
         else:
             freeTables += '1'
     db.update_seating(rid, seating)
     db.update_open_tables(rid, freeTables)
     return {'message': 'updated seating'}
 
-@app.route("/test_view_info", methods=['GET'])
+@app.route("/view_info", methods=['GET'])
 def test():
     rid = request.args.get('rid')
     print('rid=', rid)
     info = db.get_info(rid)
     return jsonify({'rest_info': info})
 
-@app.route("/test_update_free", methods=['POST'])
+@app.route("/update_free", methods=['POST'])
 def update_free_tables():
     rid = request.args.get('rid')
     freeTables = request.args.get('free')
     print(f"rid={rid} free={freeTables}")
     db.update_open_tables(rid, str(freeTables))
-    # db.update_open_tables(rid, '000010000|101000010|00000000')
     return {'message': 'updated free tables'}
     
 
